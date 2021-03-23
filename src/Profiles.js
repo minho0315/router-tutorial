5be3fd129f3feb728998da4bdffec6a00b346ff6
minho0315 <dlalsgh369@naver.com>
2021-03-23 20:44:49 +0900

    withRouter Switch  NavLink

--- src/Profiles.js
+++ src/Profiles.js
@@ -1,28 +1,40 @@
 import React from 'react';
-import {Link, Route} from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 import Profile from './Profile';
+import WithRouterSample from './WithRouterSample';
 
 const Profiles = () => {
     return (
-        <div>
-            <h3>유저 목록:</h3>
-            <ul>
-                <li>
-                    <Link to="/profiles/velopert">velopert</Link>
-                </li>
-                <li>
-                    <Link to="/profiles/gildong">gildong</Link>
-                </li>
-            </ul>
+      <div>
+        <h3>유저 목록:</h3>
+        <ul>
+          <li>
+            <NavLink
+              to="/profiles/velopert"
+              activeStyle={{ background: 'black', color: 'white' }}
+            >
+              velopert
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/profiles/gildong"
+              activeStyle={{ backgroud: "black", color: "white" }}
+            >
+              gildong
+            </NavLink>
+          </li>
+        </ul>
 
-            <Route
-            path="/profiles"
-            exact
-            render={() => <div> 유저를 선택해</div>}
-            />
-            <Route path="/profiles/:username" component={Profile} />
-        </div>
-    )
+        <Route
+          path="/profiles"
+          exact
+          render={() => <div> 유저를 선택해</div>}
+        />
+        <Route path="/profiles/:username" component={Profile} />
+        <WithRouterSample />
+      </div>
+    );
 }
 
 export default Profiles;
